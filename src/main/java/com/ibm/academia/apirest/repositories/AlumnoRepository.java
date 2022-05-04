b933f4ac26e5144b490296d4c4520cac055105a7
maraitzialonsor <mara00alro@gmail.com>
2022-05-04 17:38:27 -0500

    Update AlumnoRepository

--- src/main/java/com/ibm/academia/apirest/repositories/AlumnoRepository.java
+++ src/main/java/com/ibm/academia/apirest/repositories/AlumnoRepository.java
@@ -1,7 +1,14 @@
 package com.ibm.academia.apirest.repositories;
 
+import com.ibm.academia.apirest.entities.Persona;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("repositorioAlumnos")
-public interface AlumnoRepository extends PersonaRepository {
+public interface AlumnoRepository extends CrudRepository<Persona, Integer> {
+    @Query("select a from Alumno a where a.carrera.nombre = ?1")
+    public Iterable<Persona> buscarAlumnoPorCarrera(String nombre);
+
+    public Persona buscarAlumnoPorId(Integer id);
 }
