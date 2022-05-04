937bf7c07bad7a8f6ab9b47648bd68f344129fc0
maraitzialonsor <mara00alro@gmail.com>
2022-05-04 17:39:00 -0500

    Update CarreraRepositorio

--- src/main/java/com/ibm/academia/apirest/repositories/CarreraRepositorio.java
+++ src/main/java/com/ibm/academia/apirest/repositories/CarreraRepositorio.java
@@ -4,7 +4,18 @@ import com.ibm.academia.apirest.entities.Carrera;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// Realizando inyecciones
 @Repository
 public interface CarreraRepositorio extends CrudRepository<Carrera, Integer> {
+    // @Query("select c from Carrera c where a.cantidadAnios = ?1")
+    public Iterable<Carrera> findByCantidadAnios(Integer cantidadAnios);
 
+    // @Query("select c from Carrera c where c.nombre like %?1%")
+    public Iterable<Carrera> findCarrerasByNombreContains(String nombre);
+
+    // @Query("select c from Carrera c where upper(c.nombre like upper(%?1%)")
+    public Iterable<Carrera> findCarrerasByNombreContainsIgnoreCase(String nombre);
+
+    // @Query("select c from Carrera c where c.cantidadAnios > ?1")
+    public Iterable<Carrera> findCarrerasByCantidadAniosAfter(String cantidadAnios);
 }
