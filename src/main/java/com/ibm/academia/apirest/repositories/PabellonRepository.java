b472aa661d22ccb7244e0ab6d4062e47df32e14b
maraitzialonsor <mara00alro@gmail.com>
2022-05-13 18:36:14 -0500

    Rename PabellonRepository

--- src/main/java/com/ibm/academia/apirest/repositories/PabellonRepository.java
+++ src/main/java/com/ibm/academia/apirest/repositories/PabellonRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PabellosRepository extends CrudRepository<Pabellon, Long> {
+public interface PabellonRepository extends CrudRepository<Pabellon, Long> {
     public Iterable<Pabellon>findPabellonesByLocalidad(Direccion direccion);
     public Iterable<Pabellon>findPabellonesByNombre(String nombre);
 }
